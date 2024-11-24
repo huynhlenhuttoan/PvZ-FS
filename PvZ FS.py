a7be823f62b20fc5b7aac8abda469960877cb341
Shina <63547628+huynhlenhuttoan@users.noreply.github.com>
2024-11-24 11:08:06 +0700

    Add files via upload

--- PvZ FS.py
+++ PvZ FS.py
@@ -25,14 +25,14 @@ AminoAcids = {
     "END": {"components": ['UAA','UAG','UGA']}
 }
 
-#Plants Data: [1]: Amino Acids Code, [2]: Male:Famale ratio
+#Plants Data: [0]: Amino Acids Code, [1]: Male:Famale ratio, [2]: Plant Rating (PR)
 Plants = {
-    "Peashooter": (["DB"], [60,40]),
-    "Sunflower": (["DS"], [40,60]),
-    "Wall-nut": (["DF"], [50,50]),
-    "Pea Sunflower": (["DBS","DSB"], [50,50]), #Peashooter x Sunflower
-    "Solar-nut": (["DSF","DFS"], [30,70]), #Sunflower x Wall-nut
-    "Pea-nut": (["DGF","DBF"],[70,30])  #Peashooter x Wall-nut
+    "Peashooter": (["DB"], [60,40], 1),
+    "Sunflower": (["DS"], [40,60], 1),
+    "Wall-nut": (["DF"], [50,50], 1),
+    "Pea Sunflower": (["DBS","DSB"], [50,50], 2), #Peashooter x Sunflower
+    "Solar-nut": (["DSF","DFS"], [30,70], 2), #Sunflower x Wall-nut
+    "Pea-nut": (["DGF","DBF"],[70,30], 2)  #Peashooter x Wall-nut
 }
 
 #DNA to RNA
@@ -45,26 +45,39 @@ def RNA_to_DNA(RNA):
 
 #Separate RNA into codons
 def RNA_to_Condons(RNA):
-    codons = [RNA[i:i+3] for i in range(0,len(RNA),3)]
-    #codons = [codon for codon in codons if len(codon) == 3]
+    codons = [RNA[i:i+3] for i in range(0, len(RNA) - len(RNA) % 3, 3)]
     return codons
 
 #Translate codons to Amino Acids code
 def Codons_to_AA_code(codons):
-    ProteinCode = ""
+    print("DB:", codons)
+    Protein_Codes = []
+    start_pos = [i for i, codon in enumerate(codons) if codon == "AUG"]
+    stop_pos = [i for i, codon in enumerate(codons) if codon in AminoAcids["END"]["components"]]
 
-    for codon in codons:
-        found = False
-        for key, value in AminoAcids.items():
-            if codon in value.get("components",[]):
-                ProteinCode += value["code"]
-                found = True
-                break
+    if not start_pos or not stop_pos or all(stop < start for stop in stop_pos for start in start_pos):
+        return []  # No valid coding region
 
-            if not found and codon in AminoAcids["END"]["components"]:
-                break
 
-    return ProteinCode
+    for start in start_pos:
+        nearest_stop = next((stop for stop in stop_pos if stop > start), None) #Find nearest stop after this start codon
+
+        if nearest_stop and nearest_stop > start + 1: #Exist valid stop codon(s)
+            protein_code = ""
+            #Collect the codons between chosen start and stop codons
+            for i in range(start, nearest_stop):
+                codon = codons[i]
+                for key, value in AminoAcids.items():
+                    if "code" in value and codon in value.get("components",[]):
+                        protein_code += value["code"]
+
+            Protein_Codes.append(protein_code)
+            stop_pos = [stop for stop in stop_pos if stop > nearest_stop] #Update stop codons
+        
+        else: #No valid stop codon found after this start codon, skip this start codon
+            continue
+
+    return Protein_Codes
 
 #Translate codons to Amino Acids names
 def Codons_to_AA_name(codons):
@@ -88,12 +101,25 @@ def Codons_to_AA_name(codons):
 def get_plant_type(DNA):
     RNA = DNA_to_RNA(DNA)
     codons = RNA_to_Condons(RNA)
-    protein_code = Codons_to_AA_code(codons)
-    for plant, values in Plants.items():
-        if protein_code in values[0]:
-            return plant
+    protein_codes = Codons_to_AA_code(codons)
 
-    return "Unknown" #After checking all plants
+    highest_PR = -1
+    best_plant_type = []
+
+    for protein_code in protein_codes:
+        for plant, values in Plants.items():
+            if protein_code in values[0]: #If Protein code is valid
+                plant_PR = values[2] #Get plant's PR
+                if plant_PR > highest_PR:
+                    highest_PR = plant_PR #Update new highest PR
+                    best_plant_type = [plant] #Reset best plant type list if new higher PR was found
+                elif plant_PR == highest_PR:
+                    best_plant_type.append(plant)
+
+    if best_plant_type:
+        return random.choice(best_plant_type)
+
+    return None #After checking all plants
 
 #Get possible DNA strings (for randomizing new game starter)
 def get_possible_DNA_strings(plant_name):
@@ -140,8 +166,6 @@ def breed(plant1, plant2):
     RNA_2 = DNA_to_RNA(plant2["DNA"])
     codons_1 =  RNA_to_Condons(RNA_1)
     codons_2 = RNA_to_Condons(RNA_2)
-    print(codons_1)
-    print(codons_2)
 
     child_codons = []
 
@@ -164,12 +188,125 @@ def breed(plant1, plant2):
     child_RNA = ''.join(child_codons)
     child_DNA = RNA_to_DNA(child_RNA) 
 
+    if should_mutate(max(plant_1["IR"], plant_2["IR"]), userdata["pity"]):
+        mutated_DNA = mutate_DNA(child_DNA)
+        if mutated_DNA:
+            child_DNA = mutated_DNA
+            print("Mutation happened!")
+            userdata["pity"] = 0 #Pity became 0 if mutation happened
+        else:
+            print("Breeding failed due to corrupted mutation..")
+            userdata["pity"] += 1 #Increase pity if mutation failed
+            return None
+
     return {
         "DNA": child_DNA,
         "Protein": Codons_to_AA_name(RNA_to_Condons(DNA_to_RNA(child_DNA))),
         "type": get_plant_type(child_DNA),
-        "gender": get_random_gender(get_plant_type(child_DNA))
+        "gender": get_random_gender(get_plant_type(child_DNA)),
+        "PR": Plants[get_plant_type(child_DNA)][2],
+        "IR": Plants[get_plant_type(child_DNA)][2], #For now, PR = IR
     }
+
+#Check if mutation happens
+def should_mutate(IR, pity):
+    if random.randint(1, 100) <= IR + pity:
+        return True #Mutation happens
+    return False #No mutation
+
+def mutate_DNA(DNA):
+    mutation_type = random.choice(["Point", "Substitution", "Inversion", "Insertion", "Deletion"])
+    DNA_list = list(DNA)
+    length = len(DNA_list)
+
+    if mutation_type == "Point": #Change a nucleotide into another one
+        how_many = random.randint(1, max(1, length // 3))
+        step = 1
+        temp = []
+        while step <= how_many:
+            index = random.randint(0, length - 1)
+            if index not in temp:
+                DNA_list[index] = random.choice(["A","T","C","G"])
+                temp.append(index)
+                step += 1
+
+    elif mutation_type == "Substitution": #Change a nucleotide section into another one, with the same length
+        start = random.randint(0, length - 2)
+        end = random.randint(start + 1, length)
+        for i in range(start, end):
+            DNA_list[i] = random.choice(["A","T","C","G"])
+
+    elif mutation_type == "Inversion": #Reverse a nucleotide section
+        start = random.randint(0, length - 2)
+        end = random.randint(start + 1, length)
+        DNA_list[start:end] = reversed(DNA_list[start:end])
+
+    elif mutation_type == "Insertion": #Insert nucleotide(s) or a nucleotide section
+        how_many = random.randint(1, max(1,length // 3))
+        insert_type = random.choice(["Group", "Individual"])
+        if insert_type == "Group":
+            index = random.randint(0, length)
+            insertion = random.choices(["A","T","C","G"],k=random.randint(1, how_many))
+            DNA_list = DNA_list[:index] + insertion + DNA_list[index:]
+
+        if insert_type == "Individual":
+            step = 1
+            while step <= how_many:
+                index = random.randint(0, length)
+                base = random.choice(["A","T","C","G"])
+                DNA_list = DNA_list[:index] + base + DNA_list[index:]
+                step += 1
+
+        elif mutation_type == "Deletion": #Delete nucleotide(s) or a nucleotide section
+            delete_type = random.choice("Group", "Individual")
+            if delete_type == "Group":
+                start = random.randint(0, length - 2)
+                end = random.randint(start + 1, length)
+                del DNA_list[start:end]
+
+            if delete_type == "Individual":
+                how_many = random.randint(1, max(1, length // 3))
+                step = 1
+                while step <= how_many:
+                    if length == 0:
+                        break
+                    index = random.randint(0, length - 1)
+                    del DNA_list[index]
+                    step += 1
+
+    mutated_DNA = ''.join(DNA_list)
+    
+    #Validate the mutated DNA
+    if not is_valid_DNA(mutated_DNA):
+        return None #Mutation Failed
+    
+    print(mutated_DNA)
+    return mutated_DNA
+
+def is_valid_DNA(DNA):
+    RNA = DNA_to_RNA(DNA)
+    codons = RNA_to_Condons(RNA)
+
+    # Ensure there is a start codon
+    if "AUG" not in codons:
+        return False
+
+    try: # Ensure there is a stop codon
+        start_index = codons.index("AUG")
+    except ValueError:
+        return False  # No start codon found
+    
+    for i in range(start_index + 1, len(codons)):
+        if codons[i] in AminoAcids["END"]["components"]:
+            if i - start_index > 1: # Ensure at least one codon between start and stop
+                is_a_plant = get_plant_type(DNA)
+                if is_a_plant:
+                    return True
+                else:
+                    return False
+            break
+
+    return False  # No valid protein-coding region found
 
 #Load json file
 def load_userdata(userdata_path):
@@ -183,8 +320,13 @@ def load_userdata(userdata_path):
             return {int(k) if k.isdigit() else k: v for k, v in userdata.items()}
     
     except (json.JSONDecodeError, ValueError):
-        print("Save file is corrupted, do you want to start a new game?")
-        return create_new_userdata(userdata_path)
+        choice = input("Save file is corrupted, do you want to start a new game?\nY: Yes; N: No\n").strip().upper()
+        print(f"User input after strip and lower: '{choice}'")
+        if choice == "Y":
+            return create_new_userdata(userdata_path)
+        
+        else:
+            raise SystemExit("Exiting due to corrupted save file.")
 
 #Create new userdata   
 def create_new_userdata(userdata_path): #Give 3 starter plants
@@ -201,24 +343,31 @@ def create_new_userdata(userdata_path): #Give 3 starter plants
 
     starting_data = {
             "order_number": 4, #Already gave 3 starter plants, so the next plant will have the order number of 4
+            "pity": 0, #Mutation Pity
 
             1: {  # Peashooter
                 "DNA": random_Peashooter_DNA,
                 "Protein": Codons_to_AA_name(RNA_to_Condons(DNA_to_RNA(random_Peashooter_DNA))),
                 "type": get_plant_type(random_Peashooter_DNA),
-                "gender": peashooter_gender
+                "gender": peashooter_gender,
+                "PR": 1,
+                "IR": 1
             },
             2: {  # Sunflower
                 "DNA": random_Sunflower_DNA,
                 "Protein": Codons_to_AA_name(RNA_to_Condons(DNA_to_RNA(random_Sunflower_DNA))),
                 "type": get_plant_type(random_Sunflower_DNA),
-                "gender": sunflower_gender
+                "gender": sunflower_gender,
+                "PR": 1,
+                "IR": 1
             },
             3: {  # Wall-nut
                 "DNA": random_Wallnut_DNA,
                 "Protein": Codons_to_AA_name(RNA_to_Condons(DNA_to_RNA(random_Wallnut_DNA))),
                 "type": get_plant_type(random_Wallnut_DNA),
-                "gender": get_random_gender("Wall-nut")
+                "gender": get_random_gender("Wall-nut"),
+                "PR": 1,
+                "IR": 1
             }
         }
     
@@ -277,7 +426,7 @@ while True:
             json.dump(player_data,user_file,indent=4)
 
     except ValueError:
-        print("Invalid input. Please enter numbers!")
+        print("Invalid input. Please enter valid order numbers!")
 
     except KeyboardInterrupt:
         print("\nExiting game...")
--- README.txt
+++ README.txt
@@ -7,5 +7,12 @@ The game has biological-related cross breeding system (base on Proteins, Amino A
 Facebook: facebook.com/toannhut.1984
 Email: nhuttoan2005@gmail.com
 
+
+[Devlogs]
+
+24/11/2024 Beta 0.2
+Introducing mutation system. From now on, you can obtain fusion plants from breeding (fusion).
+3 fusion plants added: Pea Sunflower, Solar-nut, Pea-nut.
+
 20/11/2024 Beta 0.1
 The very first look of the game's breeding system. For now, there are only 3 plants that you can access: Peashooter, Sunflower and Wall-nut. There's no graphics yet, this is game mechanic testing only.
